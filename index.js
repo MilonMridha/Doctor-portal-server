447ea5048aaaeec546bef427530c968a1724d7a7
MilonMridha <milonmridha570@gmail.com>
2022-05-16 23:34:21 +0600

    add cors policy

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware--------->
-app.use(cors());
+// app.use(cors());
+//cors policy config--------->
+const corsConfig={
+    origin: true,
+    Credentials: true,
+}
+app.use(cors(corsConfig))
+app.options('*', cors(corsConfig));
+
+
 app.use(express.json())
 
 
@@ -92,7 +101,7 @@ async function run() {
             })
             res.send({ result, token });
         });
-        
+
         app.delete('/user/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
